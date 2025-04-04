5e5b032e9bea90db5eed739c01c0b29f859933e0
Dennis Gabriel B. Dimaano <dimaanodb@students.nu-lipa.edu.ph>
2025-04-04 16:15:35 +0800

    first commit

---
+++ Analyst.java
@@ -0,0 +1,12 @@
+public class Analyst extends Employee {
+    
+
+
+    public Analyst(String name, double salary, int age) {
+        super(name, age, salary);
+    }
+
+    public double getAnnualBonus() {
+        return super.salary * .05;
+    }
+}
